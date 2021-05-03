26de93beb40cd9cb55996498e6e890dcc6d9d1d3
Cosmin Dinu <cdcdd15@gmail.com>
2021-05-03 13:08:41 +0300

    Initial commit with the Java project.

---
+++ src/main/java/com/cosmin/ignite/model/EmployeeDTO.java
@@ -0,0 +1,55 @@
+package com.cosmin.ignite.model;
+
+import java.io.Serializable;
+
+import org.apache.ignite.cache.query.annotations.QuerySqlField;
+
+public class EmployeeDTO implements Serializable {
+	private static final long serialVersionUID = 1145953070573742911L;
+
+	@QuerySqlField(index = true)
+	private Integer id;
+
+	@QuerySqlField(index = true)
+	private String name;
+
+	@QuerySqlField(index = true)
+	private boolean isEmployed;
+	
+	@QuerySqlField(index = true)
+	private Integer salary;
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isEmployed() {
+		return isEmployed;
+	}
+
+	public void setEmployed(boolean isEmployed) {
+		this.isEmployed = isEmployed;
+	}
+
+	// getters, setters
+}
